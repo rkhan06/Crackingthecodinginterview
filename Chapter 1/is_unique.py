c1daaca043dca63428ff32a6044adb17a431ce1d
kingramin <rameen07@gmail.com>
2019-06-26 23:33:32 +0500

    added is_unique

---
+++ Chapter 1/is_unique.py
@@ -0,0 +1,30 @@
+import unittest
+from collections import Counter
+
+
+def is_unique(s):
+    c = Counter()
+    for i in s:
+        c[i] += 1
+        if c[i] > 1:
+            return False
+    return True
+
+
+class Test(unittest.TestCase):
+    data = [("ramen", False),
+            ("asdaffa", False),
+            ("abcdefgh", True),
+            ("isthunqe", True),
+            ("piuytreqw", True),
+            ("lkjhgfdsa", True)]
+
+    def test_unique(self):
+        for [str, output] in self.data:
+            result = is_unique(str)
+
+        self.assertEqual(result, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
