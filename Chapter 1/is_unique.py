8332839b5d7571f4322752a64af37db736ca3ac7
kingramin <rameen07@gmail.com>
2019-06-28 21:02:35 +0500

    solved  without using additional datastructure

--- Chapter 1/is_unique.py
+++ Chapter 1/is_unique.py
@@ -10,6 +10,19 @@ def is_unique(s):
             return False
     return True
 
+#### Without using additional datastructure
+
+
+def is_unique_2(s):
+    if len(s) > 128:
+        return False
+    check = [False]*128
+    for i in range(len(s)):
+        if check[ord(s[i])]:
+            return False
+        else:
+            check[ord(s[i])] = True
+    return True
 
 class Test(unittest.TestCase):
     data = [("ramen", True),
@@ -17,11 +30,12 @@ class Test(unittest.TestCase):
             ("abcdefgh", True),
             ("isthunqe", True),
             ("piuytreqw", True),
-            ("lkjhgfdsa", True)]
+            ("lkjhgfdsa", True),
+            ("asdjahkajshkjaakfabasdhaskd",False)]
 
     def test_unique(self):
         for [str, output] in self.data:
-            result = is_unique(str)
+            result = is_unique_2(str)
             self.assertEqual(result, output)
 
 
